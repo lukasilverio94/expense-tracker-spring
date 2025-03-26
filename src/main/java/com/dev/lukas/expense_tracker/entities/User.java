051d29513375f16504d03eb8d2961c34d99fb199
= <lukasilverio94@gmail.com>
2025-03-26 23:27:25 +0100

    create project

---
+++ src/main/java/com/dev/lukas/expense_tracker/entities/User.java
@@ -0,0 +1,18 @@
+package com.dev.lukas.expense_tracker.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(){
+
+    }
+
+
+
+}
