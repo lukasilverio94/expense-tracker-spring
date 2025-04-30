128be6bf07985f14fe33eb0b153aaba7f74ed325
lukasilverio <lukasilverio94@gmail.com>
2025-04-30 10:28:50 +0200

    feat: add new categories on seed

--- src/main/java/com/dev/lukas/expensetracker/config/CategoryDataLoader.java
+++ src/main/java/com/dev/lukas/expensetracker/config/CategoryDataLoader.java
@@ -36,14 +36,15 @@ public class CategoryDataLoader implements CommandLineRunner {
             Category cat14 = new Category("Sports & Gym");
             Category cat15 = new Category("Travel & Holidays");
             Category cat16 = new Category("Education & Courses");
-            Category cat17 = new Category("Work-related Expenses");
+            Category cat17 = new Category("Work");
             Category cat18 = new Category("Coffee & Snacks");
             Category cat19 = new Category("Household Items");
             Category cat20 = new Category("Pets");
+            Category cat21 = new Category("Savings");
 
             categoryRepository.saveAll(Arrays.asList(
                     cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10,
-                    cat11, cat12, cat13, cat14, cat15, cat16, cat17, cat18, cat19, cat20
+                    cat11, cat12, cat13, cat14, cat15, cat16, cat17, cat18, cat19, cat20, cat21
             ));
         }
         System.out.println(categoryRepository.count());
